0132d874f8a488d0db0528111fd15f8347f222b3
AlejandraHelipagos <alejandraelipapaluca@gmail.com>
2025-01-15 15:32:50 -0300

    add config

--- lib/getProductBySku.tsx
+++ lib/getProductBySku.tsx
@@ -1,14 +1,9 @@
 import { Product } from "@/types/types";
 
-const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://challenge-back-catalog-1.onrender.com'
-    : 'http://localhost:3000';
-
 export async function getProductBySku(sku: string): Promise<Product | string> {
 
   try {    
-    const res = await fetch(`${API_BASE_URL}/products?sku=${sku}`);
+    const res = await fetch(`https://challenge-back-catalog-1.onrender.com/products?sku=${sku}`);
 
     if (res.status === 404) return "No encontrado";
     if (res.status === 500) return "No se pudo cargar";
